826c7419ce6dc9a81918de1190916418df7c021e
MSI <MSI@MSI>
2023-11-22 17:25:14 +0300

    Добавьте файлы проекта.

---
+++ 22.11 практика задание 3 диалоговое окно/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _22._11_практика_задание_3_диалоговое_окно
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            name = Interaction.InputBox("Введите ваше имя",
+                "Ввод имени");
+            string age;
+            age = Interaction.InputBox("Введите ваш возраст",
+                "Ввод возраста");
+            string txt = "Имя: " + name + ", Возраст: " + age;
+            MessageBox.Show(txt, "Итог.");
+        }
+    }
+}
---
+++ 22.11 практика задание 3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22._11_практика_задание_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите имя: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Ведите возраст: ");
+            string age = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine($"имя: {name}, возраст: {age}");
+            Console.ReadKey();
+        }
+    }
+}
